8f8dd08e562267cab1ba71ef193ee1af7935fba1
luka <dasilva.luka97@gmail.com>
2024-10-23 15:33:04 +0200

    test

--- server.js
+++ server.js
@@ -64,6 +64,9 @@ app.use("/api/news", newsRoutes);
 app.get("/set-password", (req, res) => {
   res.sendFile(path.join(__dirname, "views/set-password.html"));
 });
+app.get("/success", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/success.html"));
+});
 
 sequelize
   .authenticate()
