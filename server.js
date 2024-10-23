9ac97408313dc3b08b6fe268baa6ce5ef0f4205a
luka <dasilva.luka97@gmail.com>
2024-10-23 10:56:22 +0200

    test

--- server.js
+++ server.js
@@ -31,7 +31,23 @@ app.use("/api/videos", videoRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = 3000;
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection to the database successful");
+    return sequelize.sync();
+  })
+  .then(() => {
+    console.log("Database synchronized");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `API documentation available at http://localhost:${PORT}/api-docs`
+  );
 });
