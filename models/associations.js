42b9c7ffcdef78440aea9a94ab3ba5f35edea955
luka <dasilva.luka97@gmail.com>
2024-10-23 10:32:41 +0200

    log

--- models/associations.js
+++ models/associations.js
@@ -1,21 +1,22 @@
-const Category = require("../models/category.model");
-const Subcategory = require("../models/subcategory.model");
-const Video = require("../models/video.model");
-const Tutorial = require("../models/tutorial.model");
+console.log("Loading Category");
+const Category = require("./category.model");
+console.log("Loading Subcategory");
+const Subcategory = require("./subcategory.model");
+console.log("Loading Video");
+const Video = require("./video.model");
+console.log("Loading Tutorial");
+const Tutorial = require("./tutorial.model");
 
-// Association entre Category et Subcategory
+// Associations
 Category.hasMany(Subcategory, { foreignKey: "category_id" });
 Subcategory.belongsTo(Category, { foreignKey: "category_id" });
 
-// Association entre Category et Video
 Category.hasMany(Video, { foreignKey: "category_id" });
 Video.belongsTo(Category, { foreignKey: "category_id" });
 
-// Association entre Subcategory et Video
 Subcategory.hasMany(Video, { foreignKey: "subcategory_id" });
 Video.belongsTo(Subcategory, { foreignKey: "subcategory_id" });
 
-// Association entre Video et Tutorial
 Video.hasOne(Tutorial, { foreignKey: "video_id" });
 Tutorial.belongsTo(Video, { foreignKey: "video_id" });
 
