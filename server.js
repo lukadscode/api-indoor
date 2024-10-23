5c67ff1b9a6ac44c3488f8354318d1255ff93279
luka <dasilva.luka97@gmail.com>
2024-10-23 11:14:12 +0200

    test

--- server.js
+++ server.js
@@ -1,54 +1,14 @@
 const express = require("express");
-const helmet = require("helmet");
-const path = require("path");
-const dotenv = require("dotenv");
-const sequelize = require("./config/db");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerOptions = require("./swagger/swaggerOptions");
-const categoryRoutes = require("./routes/category.routes");
-const subcategoryRoutes = require("./routes/subcategory.routes");
-const tutorialRoutes = require("./routes/tutorial.routes");
-const videoRoutes = require("./routes/video.routes");
-const userRoutes = require("./routes/user.routes");
-const authRoutes = require("./routes/auth.routes");
-require("./models/associations");
-console.log("Express should be defined now:", express);
 
-dotenv.config();
+console.log("Express loaded successfully");
 
 const app = express();
 
-app.use(helmet());
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
-const specs = swaggerJsdoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-
-app.use("/api/categories", categoryRoutes);
-app.use("/api/subcategories", subcategoryRoutes);
-app.use("/api/tutorials", tutorialRoutes);
-app.use("/api/videos", videoRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection to the database successful");
-    return sequelize.sync();
-  })
-  .then(() => {
-    console.log("Database synchronized");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(
-    `API documentation available at http://localhost:${PORT}/api-docs`
-  );
 });
